eea9f3771b19bddb11deda70b95222236e84b03a
Philip Anthony-Davis <philip29313@hotmail.com>
2022-08-15 20:15:22 -0400

    fixes

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -25,13 +25,13 @@ namespace SudokuSolver
             FillArray(sudoku); // Text-based input for sudoku
 
             PrintSudoku(sudoku); //Shows the initial sodoku
-            Console.WriteLine("Searching for solution... (this might take a while)"); 
+            Console.WriteLine("Searching for solution... (this might take a while)");
 
             //Recursive method that solves the sudoku
-            int[][] answer = SudokuSolver(sudoku, 0, 0); 
+            int[][] answer = SudokuSolver(sudoku, 0, 0);
 
             // If there is no solution, show error message
-            if (answer[0] == null) Console.WriteLine("\nThere is no solver for this soduku :(");            
+            if (answer[0] == null) Console.WriteLine("\nThere is no solver for this soduku :(");
             else PrintSudoku(answer); // Shows final sudoku if there is a solution
 
 
@@ -39,43 +39,46 @@ namespace SudokuSolver
         private static void FillArray(int[][] board)
         {
             //Goes through the 9x9 and asks for 9 characters
-            for(int i = 0; i < board.Length; i++)
-            {
-                if (i < 0) i = 0;
-                Console.WriteLine("Enter the line #" + (i + 1) + "(b to go back)"));
-                String input  = Console.ReadLine(); // Read the input
-                if (input.ToLower() == "b") i-= 2; // Go back if needed
-                else if (input.Length != 9) { Console.WriteLine("That input is incorrect, please try again !"); i--; } //Wrong input 
-            else for (int j = 0; j < input.Length; j++) // If input is correct, fill the board with it
-                        board[i][j] = input[j] - '0';
-                
-            }
-           
-        }
-
-        //Simple 2D printer
-        private static void PrintSudoku(int[][] board)
-        {
-            Console.WriteLine();
             for (int i = 0; i < board.Length; i++)
             {
-                for (int j = 0; j < board[i].Length; j++)
-                    Console.Write(board[i][j] + " ");
-                Console.WriteLine();
+                if (i < 0) i = 0;
+                Console.WriteLine("Enter the line #" + (i + 1) + "(b to go back)");
+                String input = Console.ReadLine(); // Read the input
+                if (input.ToLower() == "b") i -= 2; // Go back if needed
+                else if (input.Length != 9) { Console.WriteLine("That input is incorrect, please try again !"); i--; } //Wrong input 
+                else for (int j = 0; j < input.Length; j++) // If input is correct, fill the board with it
+                        board[i][j] = input[j] - '0';
             }
+
         }
+
+    
+
+    //Simple 2D printer
+    private static void PrintSudoku(int[][] board)
+    {
+        Console.WriteLine();
+        for (int i = 0; i < board.Length; i++)
+        {
+            for (int j = 0; j < board[i].Length; j++)
+                Console.Write(board[i][j] + " ");
+            Console.WriteLine();
+        }
+    }
 
 
     private static int[][] SudokuSolver(int[][] board, int x, int y)
     {
+           Console.WriteLine("x : " + x + "y : " + y);
+
         //Attempt value
         int[][] testBoard = new int[9][];
-            
+
         //2D sudoku cloning
         for (int i = 0; i < testBoard.Length; i++)
         {
             testBoard[i] = new int[9];
-            for (int j = 0; j < testBoard[i].Length; j++) 
+            for (int j = 0; j < testBoard[i].Length; j++)
                 testBoard[i][j] = board[i][j];
         }
 
@@ -115,7 +118,7 @@ namespace SudokuSolver
             var row = new List<int>();
             for (int j = 0; j < 9; ++j) { if (board[i][j] == 0) continue; row.Add(board[i][j]); }
             if (!ValidateNine(row)) return false;
-            
+
             //Column
             var col = new List<int>();
             for (int j = 0; j < 9; ++j) { if (board[j][i] == 0) continue; col.Add(board[j][i]); }
@@ -139,9 +142,9 @@ namespace SudokuSolver
         {
             if (!nums[nine.ToList()[i] - 1])
                 nums[nine.ToList()[i] - 1] = true;
-                else return false; //If they are seen more then once, return false
-            }
-            return true; //If not return true
+            else return false; //If they are seen more then once, return false
         }
+        return true; //If not return true
     }
+}
 }
